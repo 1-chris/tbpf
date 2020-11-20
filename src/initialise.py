c3b94c6d44cbb4fac50367c86f6b00f51abf7c7a
ch <chris@manymails.uk>
2020-11-20 21:02:27 +0000

    update

--- src/initialise.py
+++ src/initialise.py
@@ -4,7 +4,7 @@ conn = sqlite3.connect('tbpf-fs.db')
 
 c = conn.cursor()
 
-c.execute('''CREATE TABLE filetree (index integer, path text, iscontainer bool, origin text, baselocationid ''')
-c.execute('''CREATE TABLE tags (tagid integer, name text, taggroupid, integer)''')
+c.execute('''CREATE TABLE filetree (fileindex integer, path text, iscontainer bool, origin text, filehash text, baselocationid integer) ''')
+c.execute('''CREATE TABLE tags (tagid integer, name text, taggroupid integer)''')
 c.execute('''CREATE TABLE taggroups (taggroupid integer, name text)''')
 c.execute('''CREATE TABLE baselocations (baselocationid integer, basepath text)''')
